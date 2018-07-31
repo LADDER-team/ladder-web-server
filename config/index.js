b0bf7be4d44d0ff0e4c652434032da493f0a2ef7
tetty0217 <tettaro_01291978@ezweb.ne.jp>
2018-08-01 02:14:43 +0900

    modified package and webpack

--- config/index.js
+++ config/index.js
@@ -4,7 +4,7 @@ module.exports = {
   dev: {
     mode: 'develop',
     // Paths
-    assetsSubDirectory: 'static',
+    assetsSubDirectory: 'src/assets',
     assetsPublicPath: '/',
     proxyTable: {
       '/api/*' : {
@@ -49,7 +49,7 @@ module.exports = {
 
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
-    assetsSubDirectory: 'static',
+    assetsSubDirectory: 'assets',
     assetsPublicPath: '',
 
     /**
